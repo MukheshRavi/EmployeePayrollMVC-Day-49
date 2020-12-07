c6cc76038f38f63a74c002058ee0514d7cae0295
Pavithra <nspavi1991@gmail.com>
2020-12-07 10:24:26 +0530

    Add MVCApplication inclused index and Register page

---
+++ MVCStructureApp/Controllers/EmployeeController.cs
@@ -0,0 +1,42 @@
+﻿using MVCStructureApp.Models.Common;
+using MVCStructureApp.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCStructureApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        EmployeeRepository employeeRepository = new EmployeeRepository();
+
+        public ActionResult RegisterEmployee()
+        {
+            return View();
+        }
+        // GET: Employee
+        public ActionResult Index()
+        {
+            List<EmployeeViewModel> list = employeeRepository.GetEmployees();
+            return View(list);
+        }
+        [HttpPost]
+        public ActionResult RegisterEmployee(RegisterRequestModel employee)
+        {
+            bool result = false;
+            if (ModelState.IsValid)
+            {
+                result = employeeRepository.RegisterEmployee(employee);
+            }
+            ModelState.Clear();
+
+            if (result == true)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+    }
+}
