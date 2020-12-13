a23e82c5c9f8baf6c175a798b29adc706e06865d
MukheshRavi <mukeshparvathi@gmail.com>
2020-12-13 22:53:34 +0530

    [Mukhesh].Add UC4 and UC5 Add new Employee

--- MVCStructureApp/Controllers/EmployeeController.cs
+++ MVCStructureApp/Controllers/EmployeeController.cs
@@ -12,10 +12,31 @@ namespace MVCStructureApp.Controllers
     public class EmployeeController : Controller
     {
         EmployeeRepository employeeRepository = new EmployeeRepository();
+        public ActionResult RegisterEmployee()
+        {
+            return View();
+        }
         // GET: Employee
         public ActionResult Index()
         {
             List<EmployeeViewModel> list = employeeRepository.GetEmployees();
             return View(list);
-        } 
-    }}
+        }
+        [HttpPost]
+        public ActionResult RegisterEmployee(RegisterRequestModel employee)
+        {
+            bool result = false;
+            if (ModelState.IsValid)
+            {
+                result = employeeRepository.RegisterEmployee(employee);
+            }
+            //ModelState.Clear();
+
+            if (result == true)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+    }
+}
--- MVCStructureApp/Repository/EmployeeRepository.cs
+++ MVCStructureApp/Repository/EmployeeRepository.cs
@@ -50,5 +50,40 @@ namespace MVCStructureApp.Repository
                 throw e;
             }
         }
+        public bool RegisterEmployee(RegisterRequestModel employee)
+        {
+
+            try
+            {
+                Employee validEmployee = dbContext.Employees.Where(x => x.Name == employee.Name && x.Gender == employee.Gender).FirstOrDefault();
+                if (validEmployee == null)
+                {
+                    int departmentId = dbContext.Departments.Where(x => x.DeptName == employee.Department).Select(x => x.DeptId).FirstOrDefault();
+                    Employee newEmployee = new Employee()
+                    {
+                        Name = employee.Name,
+                        Gender = employee.Gender,
+                        DepartmentId = departmentId,
+                        SalaryId = Convert.ToInt32(employee.SalaryId),
+                        StartDate = employee.StartDate,
+                        Description = employee.Description
+                    };
+                    Employee returnData = dbContext.Employees.Add(newEmployee);
+                }
+                int result = dbContext.SaveChanges();
+                if (result > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
     }
 }
